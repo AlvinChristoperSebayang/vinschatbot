927d394b3c52aa4a8d2556d0c726baa0fbea055b
AlvinSebayang <schristoper22@gmail.com>
2024-06-04 12:02:29 +0700

    update downloaded

--- app.py
+++ app.py
@@ -2,6 +2,9 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS
 import time
 from chatbotV2 import get_answer
+import nltk
+
+nltk.download('stopwords')
 
 app = Flask(__name__)
 CORS(app)
